0a9ccf97a1867bac88379e47b5906f4ee9e3417f
Patrik Rydén <Patrik Rydn@DESKTOP-LOBA5VG>
2024-05-24 16:07:07 +0200

    Add project files.

---
+++ Data/ImageDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Molninlamning1.Models;
+
+namespace Molninlamning1.Data
+{
+    public class ImageDbContext : DbContext
+    {
+        public DbSet<Mymemories> _images { get; set; }
+
+        public ImageDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+    }
+  
+}
+
+
+
---
+++ Models/Mymemories.cs
@@ -0,0 +1,23 @@
+﻿namespace Molninlamning1.Models
+{
+    public class Mymemories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+     //   public Mymemories(int id, string name, string description, string imageUrl)
+     //   {
+     //       Id = id;
+     //       Name = name;
+     //       Description = description;
+     //       ImageUrl = imageUrl;
+     //   }
+
+        public Mymemories()
+        {
+
+        }
+    }
+}
